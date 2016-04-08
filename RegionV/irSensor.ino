123601518cf60696011aa99745d630586a873cfc
Ubuntu <jerry.shih-ming.wang@utexas.edu>
2016-04-08 14:37:40 -0500

    Fixed format of codes

--- RegionV/irSensor.ino
+++ RegionV/irSensor.ino
@@ -5,22 +5,21 @@ void setup() {
   pinMode((A3), INPUT); // front right
   pinMode((A4), INPUT); // forward
   Serial.begin(9600);
-
 }
 
 void loop() {
     Serial.print("Back left ");
     Serial.println(analogRead(A0));
-        Serial.print("Front left ");
+    Serial.print("Front left ");
     Serial.println(analogRead(A1));
-        Serial.print("Back right ");
+    Serial.print("Back right ");
     Serial.println(analogRead(A2));
-        Serial.print("Front right");
+    Serial.print("Front right");
     Serial.println(analogRead(A3));
-        Serial.print("Forward ");
+    Serial.print("Forward ");
     Serial.println(analogRead(A4));
     delay (500*4);
-  // put your main code here, to run repeatedly:
-
-
+// put your main code here, to run repeatedly:
 }
+
+
